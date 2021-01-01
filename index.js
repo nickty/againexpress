8492562cafea2ee029b3ca2fe4107f9af3fa70c9
mizan <nickty.86@gmail.com>
2021-01-01 20:11:23 +0600

    Database initailly working

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
 require('dotenv').config()
 
 const app = express()
+
+//db
+mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false})
+.then(()=>{
+    console.log('DB connected')
+})
+
+app.use(express.json());
 
 app.use('/api/users', require('./routes/users'))
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../models/User')
+
 
 router.get('/', (req, res) => {
     res.send('I am from user route')
 })
+
+router.post('/register', (req, res) => {
+
+    //console.log(req.body)
+
+    //const {name, email, password} = req.body
+    
+    const user = new User(req.body)
+
+    user.save()
+
+    res.send(user)
+    
+})
+
 
 module.exports = router
